b8747d8872e3c7b9d641ce8e3bedbe1a836647ca
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-06-26 00:07:01 +0500

    Added README.txt.

--- rcssserver3D/SimSpark/rcssserver3d/build/doc/CMakeFiles/comment.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/comment"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/comment.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
