936b07ab69c6eff42e7a72b83cd2b12439c5b892
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-07-15 14:42:37 +0500

    Implemented TConnection class in Connection.cs.

--- locomotion/WalkingAgent/WalkingAgent/Connection.cs
+++ locomotion/WalkingAgent/WalkingAgent/Connection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -119,7 +120,7 @@ namespace WalkingAgent
             return true;
         }
         
-        //
+        //Closes the socket
         public bool close()
         {
             if (!this.isActive())
@@ -130,6 +131,7 @@ namespace WalkingAgent
             return true;
         }
 
+        //Reopens the socket & initializes the socket properties
         public bool reopen()
         {
             this.close();
@@ -143,7 +145,81 @@ namespace WalkingAgent
         }
     }
 
-    class Connection
+    /*
+    @author: Emad Bin Abid
+    @date: July 15, 2018
+    -----------------------------------------------------------------------------------------------------------------------------------
+    TConnection is a class that provides methods to assign server's configuration variables, such as, PORT, IP, etc. on the client side.
+    */
+    class TConnection
     {
+        protected TDataPorter tDataPorter = null;       //Socket description of 'this' connection
+
+        //Default constructor
+        private TConnection() { }
+
+        //Overrided constructor -> Takes 'connection type', 'server's ip' & 'server's hosting port' and establishes a connection with server
+        public TConnection(ConnectionType connectionType, string serverIP = "", int serverPort = -1)
+        {
+            this.tDataPorter = new TDataPorter(connectionType);
+
+            if(serverIP != "" && serverPort != -1)
+            {
+                this.open(serverIP, serverPort);
+            }
+        }
+
+        //Returns 'true' if the connection with server has been established successfully
+        public bool isConnected()
+        {
+            return this.tDataPorter.isActive();
+        }
+
+        //Returns the 'connection mode' -> TCP/UDP
+        public ConnectionType getConnectionType()
+        {
+            return this.tDataPorter.getConnectionType();
+        }
+
+        //Returns the 'socket pointer' of current socket connection
+        public Socket getSocket()
+        {
+            return this.tDataPorter.getSocket();
+        }
+
+        //Opens a connection with server
+        public bool open(string serverIP, int serverPort)
+        {
+            this.tDataPorter.reopen();
+
+            //Binding the socket to IPEndPoint
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Parse(serverIP), serverPort);
+            this.tDataPorter.getSocket().Connect(ipEndPoint);
+
+            if (ipEndPoint == null)
+                return false;
+            return true;
+        }
+
+        //Closes the socket
+        public bool close()
+        {
+            if (!this.isConnected())
+                return false;
+            return this.tDataPorter.close();
+        }
+
+        //Sends a message via the established socket connection. Returns number of bytes of message that is sent
+        public int sendMessage(string message, int messageLength)
+        {
+            return this.tDataPorter.sendMessage(message, messageLength);
+        }
+
+        //Receives the message from server or peers. Returns the number of bytes received from the server or peers. Does not return 
+        //the actual message
+        public int receiveMessage()
+        {
+            return this.tDataPorter.receiveMessage();
+        }
     }
 }
