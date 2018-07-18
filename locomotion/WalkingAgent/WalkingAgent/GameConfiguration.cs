d1659cb6c3ec8ac68920e57efa3888cb438789b4
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-07-18 10:41:59 +0500

    Implemented GameConfiguration class in GameConfiguration.cs.

---
+++ locomotion/WalkingAgent/WalkingAgent/GameConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalkingAgent
+{
+    class GameConfiguration
+    {
+        private static GameConfiguration uniqueInstance = null;
+        private WorldModel worldModel;
+        private static string rsgPath;
+
+        public GameConfiguration()
+        {
+            worldModel = WorldModel.getUniqueInstance();
+            rsgPath = "(scene rsg/agent/nao/nao.rsg)";
+        }
+
+        public static GameConfiguration getUniqueInstance()
+        {
+            if (uniqueInstance == null)
+                uniqueInstance = new GameConfiguration();
+            return uniqueInstance;
+        }
+
+        public void init()
+        {
+            setRSGPath(rsgPath);
+        }
+
+        public static string getRSGPath()
+        {
+            return rsgPath;
+        }
+
+        public static void setRSGPath(string filePath)
+        {
+            rsgPath = filePath;
+        }
+    }
+}
