829a3836122219c4ee9ab2474721919e33651d2d
Emad Bin Abid <emad.bin.abid@gmail.com>
2018-07-24 19:40:49 +0500

    Added SexpErrCode enum in ENUM.cs.

--- locomotion/WalkingAgent/WalkingAgent/ENUM.cs
+++ locomotion/WalkingAgent/WalkingAgent/ENUM.cs
@@ -125,6 +125,62 @@ namespace WalkingAgent
         llj6
     }
 
+    /*Error codes used by the library are defined in this enumeration.They
+    are either used as values for the error field within the continuation
+    structures, or as return values for functions with a return type of
+    sexp_errcode_t.*/
+    enum SexpErrCode
+    {
+        /*No Error*/
+        SEXP_ERR_OK,
+        
+        /*Memory Error.  malloc/realloc/calloc failures may result in this error
+        code. This can either result from the system calls failing, or in the
+        limited memory mode of the library, the memory limit being exceeded.
+        In limited memory mode, if this error condition is present, one should
+        check the memory limits that were in place during the erroneous call.*/
+        SEXP_ERR_MEMORY,
+
+        /*Badly Formed Expression. Missing, misplaced, or mismatched parenthesis will result in this error.*/
+        SEXP_ERR_BADFORM,
+
+        /*A sexp_t that is inconsistent will result in this error code. 
+          An example is a SEXP_BASIC sexp_t with a null val field but a non-zero val_used value. 
+          Similar cases exist for SEXP_DQUOTE, SQUOTE, and BINARY types.
+          This value is also used in the parser to flag a case where an inlined binary block is given a negative length.*/
+        SEXP_ERR_BADCONTENT,
+
+        /*If a null string is passed into the parser, this error occurs.*/
+        SEXP_ERR_NULLSTRING,
+
+        /*General IO related errors, such as failure of fopen(). 
+        These are basically non-starters with respect to getting the IO routines going.*/
+        SEXP_ERR_IO,
+
+        /*I/O routines that return NULL may simply have nothing to read. 
+        This is sometimes an error condition if the io wrapper continuation contains a partially complete s-expression, 
+        but nothing more is present (yet) on the file descriptor.*/
+        SEXP_ERR_IO_EMPTY,
+
+        /*When running the library under limited memory (ie, _SEXP_LIMIT_MEMORY_ defined at build time), 
+        this error will be produced when the memory limit is exceeded.*/
+        SEXP_ERR_MEM_LIMIT,
+
+        /*Buffer for unparsing is full.*/
+        SEXP_ERR_BUFFER_FULL,
+
+        /*routines that take parameters such as memory limits, growth sizes, or default sizes, 
+        can produce this error if a bad value has been passed in. This error usually will indicate that the parameters were bad and the
+        default values were used instead (ie, it is non-fatal.).*/
+        SEXP_ERR_BAD_PARAM,
+
+        /*Bad stack state encountered.*/
+        SEXP_ERR_BAD_STACK,
+
+        /*Unknown parser state*/
+        SEXP_ERR_UNKNOWN_STATE
+    }
+
     class ENUM
     {
         
